38aaa818380a40572a9e142be34150677540dc08
Kwan Ting Lau <kestino@iMac.local>
2023-03-03 09:31:54 -0500

    Added fancyhoverwordspread

---
+++ Fancy-Hover-Word-Spread/script.js
@@ -0,0 +1,13 @@
+const enhance = id => {
+    const ele = document.getElementById(id), text = ele.innerText.split("");
+    ele.innerText = "";
+    text.forEach(letter => {
+        const span = document.createElement("span");
+        span.className = "letter";
+        span.innerText = letter;
+        ele.appendChild(span);
+    });
+};
+
+enhance("tag1");
+enhance("tag2");
